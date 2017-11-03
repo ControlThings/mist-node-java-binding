4ebc35b01c56ae90ccc4417c85de015fcb1b9685
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2017-11-03 10:07:18 +0200

    moved node from mist-node-android

---
+++ MistException.java
@@ -0,0 +1,11 @@
+package mistNode.node;
+
+/**
+ * Created by jan on 10/19/17.
+ */
+
+class MistException extends RuntimeException {
+    public MistException(String s) {
+        super(s);
+    }
+}
